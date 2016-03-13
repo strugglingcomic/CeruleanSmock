d7a8de5a73ced4f00ef7e843bcb21bfc775a387e
Jeffrey King <nerdpride4life@gmail.com>
2016-03-13 17:56:20 -0400

    Update Blue Apron Crawler to take multiple URLS at once and output JSON

--- crawler/blueApronParser.py
+++ crawler/blueApronParser.py
@@ -1,3 +1,5 @@
+# Parses blue_apron_raw data to give array of Blue Apron Links
+
 recipeArray = []
 
 from bs4 import BeautifulSoup
@@ -14,6 +16,8 @@ def findAllLinks():
 		newLink = "https://www.blueapron.com" + link['href']
 		print newLink
 		recipeArray.append(newLink)
+	with open('blueApronLinks.json', 'w') as outfile:
+		json.dump(recipeArray, outfile)
 
 findAllLinks();
 print recipeArray
